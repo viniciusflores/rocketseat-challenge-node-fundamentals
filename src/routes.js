cf84837f24c8bfe353287d23881eff021ea39d01
Vinicius Flores <viniciusflores379@gmail.com>
2023-02-04 16:07:32 -0300

    complete delete and patch

--- src/routes.js
+++ src/routes.js
@@ -56,11 +56,11 @@ export const routes = [
     method: 'PUT',
     path: buildRoutePath('/tasks/:id'),
     handler: (req, res) => {
-      const {id} = req.params
-      const task = database.select('tasks', {id})
+      const { id } = req.params
+      const task = database.select('tasks', { id })
 
       if(task.length === 0){
-        return res.writeHead(404).end('User not found')
+        return res.writeHead(404).end('Task not found')
       }
 
       const { title, description } = req.body
@@ -81,7 +81,16 @@ export const routes = [
     method: 'DELETE',
     path: buildRoutePath('/tasks/:id'),
     handler: (req, res) => {
-      console.log('delete')
+      const { id } = req.params
+
+      const task = database.select('tasks', { id })
+
+      if(task.length === 0){
+        return res.writeHead(404).end('Task not found')
+      }
+
+      database.delete('tasks', id)
+
       return res.writeHead(204).end()
     }
   },
@@ -89,9 +98,23 @@ export const routes = [
     method: 'PATCH',
     path: buildRoutePath('/tasks/:id/complete'),
     handler: (req, res) => {
-      console.log('patch')
-      console.log('DATABASE IS NOT IMPLEMENTED')
-      return res.writeHead(204).end()
+      const { id } = req.params
+
+      const task = database.select('tasks', { id })
+
+      if(task.length === 0){
+        return res.writeHead(404).end('Task not found')
+      }
+
+      const updatedTask = {
+        ...task[0],
+        completed_at: task[0].completed_at === null ? Date.now() : null,
+        updated_at: Date.now()
+      }
+
+      database.update('tasks', id, updatedTask)
+
+      return res.writeHead(200).end()
     }
   },
 ]
